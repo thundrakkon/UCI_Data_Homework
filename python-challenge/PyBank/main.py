1e74bb9fff519c91bc3b92122a61449f85bb51ec
thundrakkon <DuongPTNguyen@gmail.com>
2020-04-04 03:24:15 -0700

    Changes

--- python-challenge/PyBank/main.py
+++ python-challenge/PyBank/main.py
@@ -4,5 +4,15 @@ import csv
 
 pybank_csv = os.path.join("Resources", "budget_data.csv")
 
+print("Financial Analysis")
+print("----------------------------")
+
+#count months in data
+count_months = 0
 with open(pybank_csv) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
+    csvreader = csv.reader(csvfile)
+    if csv.Sniffer().has_header:
+        next(csvreader)
+    for row in csvreader:
+        count_months += 1
+print(f"Total Months: {count_months}")
